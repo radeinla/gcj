d899e9b23a947f51515ca1d4c02ba8c8820fc9c6
Reginald Eli A. Deinla <radeinla@gmail.com>
2014-05-11 18:07:57 +0800

    round 1b

---
+++ 2014/round_1b/c.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import sys
+
+def get_file(argv):
+    if len(argv) == 1:
+        return "a.in"
+    else:
+        return "a_%s.in" % (argv[1])
+
+def get_file_out(argv):
+    if len(argv) == 1:
+        return "a.out"
+    else:
+        return "a_%s.out" % (argv[1])
+
+def print_answer(t, answer, f):
+    answer = "Case #%d: %s" % (t, answer)
+    print answer
+    f.write(answer)
+    f.write("\n")
+
+def main(argv):
+    pass
+
+if __name__ == "__main__":
+    main(sys.argv)
