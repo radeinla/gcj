de8beb00473176b909c68fbde1f62a7450a260f3
Reginald Eli A. Deinla <radeinla@gmail.com>
2014-05-11 18:08:09 +0800

    Problem A small

---
+++ 2014/round_1c/a.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+
+import sys
+
+from fractions import gcd
+
+def get_file(argv):
+    if len(argv) == 1:
+        return "a.in"
+    else:
+        return "a_%s.in" % (argv[1])
+
+def get_file_out(argv):
+    if len(argv) == 1:
+        return "a.out"
+    else:
+        return "a_%s.out" % (argv[1])
+
+def print_answer(t, answer, f):
+    answer = "Case #%d: %s" % (t, answer)
+    print answer
+    f.write(answer)
+    f.write("\n")
+
+cache = {}
+
+def min_gens(P, Q, d, mx):
+    # g = gcd(P, Q)
+    # print P, Q, g
+    # P = P/g
+    # Q = Q/g
+    key = '%d/%d' % (P, Q)
+    if cache.has_key(key):
+        return cache[key]
+    else:
+        ans = None
+        # print ' '*d, P, Q
+        if d > mx:
+            ans = None
+        elif 2 * P == Q:
+            # print 'got it', P, '/', Q
+            ans = d
+        elif 2 * P > Q:
+            if 2 * P > 2 * Q:
+                # print 'weird orgy???'
+                ans = None
+            else:
+                if min_gens(2*P-Q, Q, d+1, 40-d):
+                    ans = d
+                else:
+                    ans = None
+        else:
+            # print 'h'
+            ans = min_gens(P * 2, Q, d+1, 40)
+        cache[key] = ans
+        return cache[key]
+
+def main(argv):
+    f = open(get_file(argv), 'r')
+    f_out = open(get_file_out(argv), 'w')
+    T = int(f.readline())
+    for case in xrange(1, T+1):
+        P, Q = [int(x) for x in f.readline().strip().split('/')]
+        ans = min_gens(P, Q, 1, 40)
+        if ans:
+            print_answer(case, ans, f_out)
+        else:
+            print_answer(case, 'impossible', f_out)
+
+if __name__ == "__main__":
+    main(sys.argv)
