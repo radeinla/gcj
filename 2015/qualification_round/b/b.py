2675ffe6f0da361603488d4285903bde0fcd1cc0
Reginald Eli A. Deinla <radeinla@gmail.com>
2015-04-11 22:04:21 +0800

    Working version but slow.

--- 2015/qualification_round/b/b.py
+++ 2015/qualification_round/b/b.py
@@ -27,31 +27,29 @@ def print_answer(t, answer, f):
 def min_steps(diners, steps, current_min):
     # print ''.join([' ']*steps), steps, diners, current_min
     left = sum(diners)
-    if current_min is not None and steps >= current_min:
+    if steps >= current_min:
         return current_min
     if left == 0:
         return steps
 
-    diners_with_special = [diner for diner in diners if diner > 0]
+    current_diners = [diner for diner in diners if diner > 0]
     max_index = 0
     max_pancakes = 0
-    for i in xrange(0, len(diners_with_special)):
-        if max_pancakes < diners_with_special[i]:
-            max_pancakes = diners_with_special[i]
+    for i in xrange(0, len(current_diners)):
+        if max_pancakes < current_diners[i]:
+            max_pancakes = current_diners[i]
             max_index = i
     if max_pancakes == 1:
         return steps + 1
-    diners_with_special[max_index] = diners_with_special[max_index]/2
-    diners_with_special.append(max_pancakes - diners_with_special[max_index])
-    steps_with_special = min_steps(diners_with_special, steps+1, current_min)
-    if current_min is None:
-        current_min = steps_with_special
-    else:
+    for to_transfer in xrange(1, ((max_pancakes+1)/2)+1):
+        diners_with_special = current_diners[:]
+        diners_with_special[max_index] = current_diners[max_index] - to_transfer
+        diners_with_special.append(to_transfer)
+        steps_with_special = min_steps(diners_with_special, steps+1, current_min)
         current_min = min(steps_with_special, current_min)
 
     diners_without_special = [diner-1 for diner in diners if diner > 0]
-    steps_without_special = min_steps(diners_without_special, steps+1, current_min)
-    return min(steps_with_special, steps_without_special)
+    return min_steps(diners_without_special, steps+1, current_min)
 
 def main(argv):
     with open(get_file(argv), 'r') as f, open(get_file_out(argv), 'w') as f_out:
@@ -59,8 +57,8 @@ def main(argv):
         for t in xrange(1, T+1):
             N = int(f.readline())
             initial = [int(x) for x in f.readline().split(' ')]
-            current_pancakes = sum(initial)
-            print_answer(t, "%d" % min_steps(initial, 0, None), f_out)
+            current_pancakes = max(initial)
+            print_answer(t, "%d" % min_steps(initial, 0, current_pancakes), f_out)
 
 if __name__ == "__main__":
     main(sys.argv)
