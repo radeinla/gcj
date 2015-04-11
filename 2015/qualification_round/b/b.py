a7f25c1e5b9145cc8f7d00b6a887bb3de6e0848c
Reginald Eli A. Deinla <radeinla@gmail.com>
2015-04-12 00:43:56 +0800

    faster implementation via breadth-first

--- 2015/qualification_round/b/b.py
+++ 2015/qualification_round/b/b.py
@@ -24,33 +24,28 @@ def print_answer(t, answer, f):
     f.write(answer)
     f.write("\n") 
 
-def min_steps(diners, steps, current_min):
-    # print ''.join([' ']*steps), steps, diners, current_min
-    left = sum(diners)
-    if steps >= current_min:
-        return current_min
-    current_diners = [diner for diner in diners if diner > 0]
-    if not current_diners:
-        return steps
-
-    current_diners = [diner for diner in diners if diner > 0]
-    max_index = 0
-    max_pancakes = 0
-    for i in xrange(0, len(current_diners)):
-        if max_pancakes < current_diners[i]:
-            max_pancakes = current_diners[i]
-            max_index = i
-    if max_pancakes == 1:
-        return steps + 1
-    for to_transfer in xrange(1, ((max_pancakes+1)/2)+1):
-        diners_with_special = current_diners[:]
-        diners_with_special[max_index] = current_diners[max_index] - to_transfer
-        diners_with_special.append(to_transfer)
-        steps_with_special = min_steps(diners_with_special, steps+1, current_min)
-        current_min = min(steps_with_special, current_min)
-
-    diners_without_special = [diner-1 for diner in diners if diner > 0]
-    return min_steps(diners_without_special, steps+1, current_min)
+def min_steps(diners):
+    steps = 0
+    current = set([tuple(sorted(diners))])
+    nexts = set()
+    while current or nexts:
+        if not current:
+            current.update(nexts)
+            nexts.clear()
+            steps = steps + 1
+        current_diners = current.pop()
+        if not current_diners or current_diners[-1] <= 0:
+            break
+        regular = tuple([diner-1 for diner in current_diners if diner-1>0])
+        nexts.add(regular)
+        mx = current_diners[-1]
+        for i in xrange(1, (mx/2)+1):
+            special = [diner for diner in current_diners]
+            special[-1] = i
+            special.append(mx-i)
+            special.sort()
+            nexts.add(tuple(special))
+    return steps
 
 def main(argv):
     with open(get_file(argv), 'r') as f, open(get_file_out(argv), 'w') as f_out:
@@ -59,7 +54,7 @@ def main(argv):
             N = int(f.readline())
             initial = [int(x) for x in f.readline().split(' ')]
             current_pancakes = max(initial)
-            print_answer(t, "%d" % min_steps(initial, 0, current_pancakes), f_out)
+            print_answer(t, "%d" % min_steps(initial), f_out)
 
 if __name__ == "__main__":
     main(sys.argv)
