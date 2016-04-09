b676706bf055eb648c6abad829f5b64642b0cba3
Reginald Eli Deinla <radeinla@softwarelab7.com>
2016-04-09 21:43:49 +0800

    Working solution for problem A.

---
+++ 2016/qualification_round/a/a.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import sys
+
+def get_file(argv):
+    if len(argv) == 1:
+        return "a.in"
+    else:
+        return "a_%s.in" % (argv[1])
+
+def get_file_out(argv):
+    if len(argv) == 1:
+        return "a.out"
+    else:
+        return "a_%s.out" % (argv[1])
+
+def print_answer(t, answer, f):
+    answer = "Case #%d: %s" % (t, answer)
+    print answer
+    f.write(answer)
+    f.write("\n")
+
+def get_last_number(N):
+    past_numbers = set()
+    goal = set([i for i in xrange(1, 10)])
+    found = set()
+    multiplier = 1
+    current_number = N
+    while current_number not in past_numbers:
+        digits = [int(d) for d in str(current_number)]
+        found.update(digits)
+        if len(found) == 10:
+            return current_number
+        past_numbers.add(N)
+        multiplier = multiplier + 1
+        current_number = N * multiplier
+    return None
+
+def main(argv):
+    f = open(get_file(argv), 'r')
+    f_out = open(get_file_out(argv), 'w')
+    T = int(f.readline())
+    for t in xrange(1, T+1):
+        N = int(f.readline())
+        last_number = get_last_number(N)
+        if last_number is None:
+            last_number = "INSOMNIA"
+        print_answer(t, last_number, f_out)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
